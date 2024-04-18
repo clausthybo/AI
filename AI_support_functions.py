86737cf82e1a3ab32e069738e6a4b2ac6afdd6c5
clausthybo <73294792+clausthybo@users.noreply.github.com>
2024-04-18 21:54:29 +0200

    Update AI_support_functions.py

--- AI_support_functions.py
+++ AI_support_functions.py
@@ -5,7 +5,7 @@ def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
                optimizer:torch.optim.Optimizer,
-               device=device):
+               device:device):
   model.train()
   train_loss, train_acc= 0, 0
 
